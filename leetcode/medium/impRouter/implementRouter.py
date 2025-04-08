2a6494b17935383aa7d7a2aa6cca234b7bd4b4a6
Alaayed <salaayed@purdue.edu>
2025-04-08 09:28:40 -0400

    Completed router

--- leetcode/medium/impRouter/implementRouter.py
+++ leetcode/medium/impRouter/implementRouter.py
@@ -1,12 +1,14 @@
+from collections import defaultdict, deque
 from queue import Queue
 from typing import List
-
+from bisect import bisect_left, bisect_right, insort
 
 class Packet:
 	def __init__(self, source: int, destination: int, timestamp: int):
 		self.source = source
 		self.destination = destination
 		self.timestamp = timestamp
+
 	def to_list(self):
 		return [self.source, self.destination, self.timestamp]
 
@@ -16,38 +18,54 @@ class Packet:
 		return (self.source == other.source and
 		        self.destination == other.destination and
 		        self.timestamp == other.timestamp)
+
+	def __hash__(self):
+		# You can hash a tuple of the attributes
+		return hash((self.source, self.destination, self.timestamp))
+
+
 class Router:
 	def __init__(self, memoryLimit: int):
-		self.memory = []
+		self.memory = deque()
+		self.hashMap = defaultdict(lambda: False)
 		self.memoryLimit = memoryLimit
+		self.sorted_time = defaultdict(list)
+
 	def addPacket(self, source: int, destination: int, timestamp: int) -> bool:
 		# Create the packer
 		packet = Packet(source, destination, timestamp)
 		# Already in memory
 		# print(f"packet {packet}, in memory {packet in self.memory}")
-		if packet in self.memory:
+		if self.hashMap[packet]:
 			return False
 		# Already full
 		if len(self.memory) == self.memoryLimit:
-			self.memory.pop(0)
-			self.memory.append(packet)
-			return True
+			# Oldest packet from memory, remove timestamp, and hash map
+			rem = self.memory.popleft()
+			idx = bisect_left(self.sorted_time[rem.destination], rem.timestamp)
+			if idx <= len(self.sorted_time[rem.destination]):
+				self.sorted_time[rem.destination].pop(idx)
+			self.hashMap[rem] = False
+		# add to memory, timeStamp and hashMap
 		self.memory.append(packet)
+		self.hashMap[packet] = True
+		insort(self.sorted_time[destination] , timestamp)
 		return True
 
 	def forwardPacket(self) -> List[int]:
-		if len(self.memory) != 0:
-			packet = self.memory.pop(0)
+		if self.memory:
+			packet = self.memory.popleft()
+			self.hashMap[packet] = False
+			idx = bisect_left(self.sorted_time[packet.destination], packet.timestamp)
+			if idx <= len(self.sorted_time[packet.destination]):
+				self.sorted_time[packet.destination].pop(idx)
 			return packet.to_list()
 		return []
 
 	def getCount(self, destination: int, startTime: int, endTime: int) -> int:
-		inc = 0
-		for packet in self.memory:
-			timeStamp = packet.timestamp
-			if startTime <= timeStamp <= endTime and packet.destination == destination:
-				inc+=1
-		return inc
+		tList = self.sorted_time[destination]
+		return bisect_right(tList, endTime) -bisect_left(tList, startTime)
+
 
 # Your Router object will be instantiated and called as such:
 # obj = Router(memoryLimit)
@@ -57,6 +75,6 @@ class Router:
 
 
 router = Router(3)
-router.addPacket(1,4,90)
-router.addPacket(2,5,90)
-router.addPacket(1,4,90)
+router.addPacket(1, 4, 90)
+router.addPacket(2, 5, 90)
+router.addPacket(1, 4, 90)

4f8a27f2d11dd6a4381229a91a1a73b9ed531bb8
Alaayed <salaayed@purdue.edu>
2025-04-16 17:21:58 -0400

    Revert "Okay, figured out the issue. Need to implement recursion WITH pruning. Without recursion---memory limit exceeded. Without pruning---time limit exceeded. Transform current dp imp into dfs + memoization"
    This reverts commit 1c78d861247e92179755f06044935f65eb8cf0bb.

--- leetcode/hard/84LargestRectangle/lrih.py
+++ leetcode/hard/84LargestRectangle/lrih.py
@@ -1,41 +1,28 @@
 import bisect as bi
 from typing import List
-debug = False
+
 
 class Solution:
 	def largestRectangleArea(self, heights: List[int]) -> int:
 		max_area = 0
-		stack = [(heights[0], 0)]
+		stack = [].append((heights[0],0))
 		for i,h in enumerate(heights):
 			# When we encounter h < stack[-1][0]
-
-			if debug: print(f'stack: {stack}')
-			if debug: print(f'h < stack: {h < stack[-1][0]} h: {h} stack: {stack[-1][0]}' )
-			if stack and h < stack[-1][0]:
-				# pop the stack until condition fails
-				last_pop = 0
-				while stack and h < stack[-1][0]:
-					# Get height and index
+			if stack and h < stack[-1][1]:
+				# pop the stack until we reach a
+				while stack and h < stack[-1][1]:
 					height, index =stack.pop()
-					width = i - index
-					last_pop = index
-					max_area = max(max_area, height * width )
-				# start from the last popped bar.
-				stack.append((h, last_pop))
+					max_area = max(max_area, height * index)
+
 			else:
 				stack.append((h,i))
-		while stack:
-			height, index =stack.pop()
-			width = len(heights) - index
-			max_area = max(max_area, height * width)
-			if debug: print(f'height, index: {height}, {index}. width: {width}, max_area: {max_area}')
 		return max_area
 
 
 sol = Solution()
-print(sol.largestRectangleArea([2,1,5,6,2,3]))
-print(sol.largestRectangleArea([2,4]))
+sol.largestRectangleArea([2,1,5,6,2,3])
+sol.largestRectangleArea([2,4])
 print(f'sol : {sol.largestRectangleArea([1,1])}')
-print(f'sol : {sol.largestRectangleArea([4,2,0,3,2,5])}')
+
 # # #
 

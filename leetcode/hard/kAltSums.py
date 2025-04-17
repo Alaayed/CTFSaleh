69f69cb22bad7698c70abe27b8653954206129fe
Alaayed <salaayed@purdue.edu>
2025-04-17 12:35:01 -0400

    Added the function def, will work on it later

--- leetcode/hard/kAltSums.py
+++ leetcode/hard/kAltSums.py
@@ -2,50 +2,22 @@ import ast
 from typing import List, Tuple
 
 
-def check_condition(pair: Tuple[int,int,int], k: int, limit: int) -> int:
-	if pair[0] == k and pair[1] <= limit:
-		return pair[1]
-	else:
-		return -1
-
-
+def rec(index: int, 
+		sum: int,
+		prod: int,
+		k: int,
+		nums: List[int],
+		isOdd: bool) -> int:	
+	# for now, just implement grabbing all elements	
+	
+	sign = -1 if isOdd else 1
+	ans = rec(index+1,sum + nums[index] * sign, prod * nums[index], k, nums , not isOdd)
+	return ans;
 class Solution:
 	def maxProduct(self, nums: List[int], k: int, limit: int) -> int:
-		max_prod = -1
-		# dp[i] all alternating sum product pairs starting from i
-		# dp[i] = (dp[i+1]*-1 + nums[i] , dp[i+1]*nums[i])
-		dp = [[] for _ in range(len(nums))]
+		return 0;		
 
-		# Base Case
-		dp[-1] = [(nums[-1], nums[-1], -1)]
-		max_prod = max(max_prod, check_condition(dp[-1][0], k, limit))
-		# Write out the definitions
-		for i in range(2, len(nums) + 1):
-			val = nums[-i]
-			# definitions reliant of prev entry
-
-			for sum, product, prev_product in dp[-i + 1]:
-				included = (val - sum, val * product, product)
-				# skip i+1'th element
-				alt_sum = sum - nums[-i + 1]
-				# 2 - 3 + 4 -> -3 + 4
-				not_included = (val + alt_sum, val * prev_product, prev_product)
-				# skip if product > limit
-				if included[1] <= limit:
-					dp[-i].append(included)
-				if not_included[1] <= limit:
-					dp[-i].append( not_included )
-				# Check for max prod
-				max_prod = max(max_prod, check_condition(included, k, limit))
-				max_prod = max(max_prod, check_condition(not_included, k, limit))
-
-			# Add base
-			base = (val, val, -1)
-			dp[-i].append(base)
-			max_prod = max(max_prod, check_condition(base, k, limit))
-		return int(max_prod)
-
-
+	
 sol = Solution()
 import ast
 

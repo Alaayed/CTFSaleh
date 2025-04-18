49452b346f1b7818ea76b4b4ca40e0c313e9e65d
saleh-alaayed <salaayed@purdue.edu>
2025-04-18 00:11:34 -0400

    I got it, but damn was my solution unoptimal. So crazy guy was doing bit manipulation to solve this problem. ion even understand his solution. I'll see tomorrow if I want to try optimizing this or moving.

--- leetcode/hard/kAltSums.py
+++ leetcode/hard/kAltSums.py
@@ -1,39 +1,44 @@
 import ast
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
+def rec(k: int, limit: int, index: int, current_sum: int, product: int, isOdd: int, nums: List[int], memo: Dict = {} ) -> int:
+    key = (index, current_sum, product, isOdd)
+    if key in memo:
+        return memo[key]
+    # base case
+    if index == len(nums):
+        if current_sum == k and product <= limit and isOdd != 0:
+            return product
+        else:
+            return -1
+    # here is the case where we skip
+    skip = rec(k=k , limit=limit , index=index+1 ,current_sum=current_sum , product=product , isOdd=isOdd, nums=nums, memo=memo)
+    # Starting the sequence
+    nskip = -1
+    if isOdd == 0:
+        # Start sum from index
+        current_sum = nums[index]
+        product     = nums[index]
+        nskip = rec(k=k , limit=limit , index=index+1 ,current_sum=current_sum , product=product , isOdd=2, nums=nums,memo=memo)
+    elif isOdd == 1:
+        # Continue sum from index
+        current_sum = current_sum + nums[index]
+        product = product * nums[index]
+        product = min(limit+1, product)
+        nskip = rec(k=k , limit=limit , index=index+1 ,current_sum=current_sum , product=product , isOdd=2, nums=nums,memo=memo)
+    elif isOdd == 2:
+        # Continue sum from index
+        current_sum = current_sum - nums[index]
+        product = product * nums[index]
+        product = min(limit+1, product)
+        nskip = rec(k=k , limit=limit , index=index+1 ,current_sum=current_sum , product=product , isOdd=1, nums=nums,memo=memo)
+    memo[key] = max(skip, nskip)
+    return memo[key]
 
-def rec(index: int, 
-		sum: int,
-		prod: int,
-		k: int,
-		nums: List[int],
-		isOdd: bool) -> int:	
-	# for now, just implement grabbing all elements	
-	
-	sign = -1 if isOdd else 1
-	ans = rec(index+1,sum + nums[index] * sign, prod * nums[index], k, nums , not isOdd)
-	return ans;
 class Solution:
 	def maxProduct(self, nums: List[int], k: int, limit: int) -> int:
-		return 0;		
+	   return rec(k=k, limit=limit, index=0 , current_sum=0, product=0, isOdd=0, nums = nums,memo={} );
 
-	
+
 sol = Solution()
-import ast
-
-data = []
-
-with open('kAltSums.txt', 'r') as f:
-	lines = [line.strip() for line in f]
-
-t = int(lines[0])  # Number of test cases
-
-for i in range(1, t):
-	idx = 1 + i * 3
-	my_list = ast.literal_eval(lines[idx])
-	num1 = int(lines[idx + 1])
-	num2 = int(lines[idx + 2])
-	data.append((my_list, num1, num2))
-
-for nums, k, limit in data:
-	print(sol.maxProduct(nums, k, limit))
+print(sol.maxProduct([0,2,3], -5 , 12))

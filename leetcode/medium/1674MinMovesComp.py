be31b3dba38dc5ecf89d5803100290a1e8d2f807
saleh-alaayed <salaayed@purdue.edu>
2025-04-20 00:48:40 -0400

    need to change my approach for MinMovesComp to a dp approach. Current one is over counting for some reason

--- leetcode/medium/1674MinMovesComp.py
+++ leetcode/medium/1674MinMovesComp.py
@@ -7,31 +7,39 @@ class Solution:
         maxFreq = -1
         # find most frequent complement
         n = len(nums)
+        minMoves = n+1
         for i in range ( len(nums) // 2):
             comp = nums[i] + nums[n-1-i]
-            compFreq[comp] = True
-        # find the comp with the max
-        minMoves = n+1
-        for comp in compFreq:
             minMoves = min(minMoves,self.computeMinMoves(comp, limit, nums))
+            print(f'comp: {comp}, minMoves: {minMoves}')
         return minMoves
     def computeMinMoves(self, comp: float, limit: int, nums: List[int]) -> int:
         moves = 0
         n = len(nums)
         for i in range (len(nums) // 2):
             otherComp = nums[i] + nums[n-1-i]
-            diff = abs(otherComp - comp)
+            diff = comp - otherComp
             if diff != 0:
-                if (limit - nums[i]) >= diff or (limit-nums[n-1-i]) >= diff:
+                upwardOne = limit-nums[i]
+                upwardTwo = limit-nums[n-1-i]
+                downWardOne = nums[i]-1
+                downWardTwo = nums[n-1-i]-1
+                if diff >= 0 and ( upwardOne - diff >= 0 or upwardTwo - diff >= 0):
+                    moves += 1
+                elif diff < 0 and(downWardOne + diff >= 0 or downWardTwo + diff >= 0):
                     moves += 1
                 else:
+                    print(f'i: [{i}]')
                     moves += 2
         return moves
-    
+
 sol = Solution()
 nums = [1,2,4,3]
 limit = 4
 print(sol.minMoves(nums , limit))
 nums=[1,2,2,1]
 limit =2
+print(sol.minMoves(nums , limit))
+nums=[20744,7642,19090,9992,2457,16848,3458,15721]
+limit = 22891
 print(sol.minMoves(nums , limit))

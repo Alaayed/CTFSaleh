7610b4e72d3ab52b31bedec659899832cfdb03a7
saleh-alaayed <salaayed@purdue.edu>
2025-04-26 21:30:58 -0400

    Finished max points on line, solution just needed to account for vertical and horizontal lines and add a tolerance intercept condition

--- leetcode/hard/maxPointsOnLine.py
+++ leetcode/hard/maxPointsOnLine.py
@@ -1,29 +1,31 @@
-from sys import intern
 from typing import List, Set, Tuple
+from collections import defaultdict
+from fractions import Fraction
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         lines = self.getLines(points)
         best= 1
-        print(f'lines : {lines}')
         for s, i in lines:
             onLine = 0
             for p in points:
-                # y-mx = b
-                onLine += 1 if (p[1] - s*p[0]) == i else 0
+                if abs(p[1] - (s * p[0] + i)) < 1e-9:
+                    onLine += 1
             best = max(onLine , best)
+
         # check horizontal and vertical lines
-        hor = 0
-        vert = 0
+        hor = defaultdict(int)
+        vert = defaultdict(int)
         for p in points:
-            vert += 1 if p[1] == 0 else 0
-            hor  += 1 if p[0] == 0 else 0
-        best = max(best , hor)
-        best = max(best, vert)
+            hor[p[0]] += 1
+            vert[p[1]] += 1
+        for _,h in hor.items():
+            best = max(best , h)
+        for _,v in vert.items():
+            best = max(best, v)
         return best
     def getLines(self, points: List[List[int]]) -> Set[Tuple[int,int]]:
         n = len(points)
         origin = False
-        print(f'points : {points}')
         lines = set()
         for i in range(n):
             p1 = points[i]
@@ -32,17 +34,19 @@ class Solution:
                 origin = origin or (p2 == [0,0] or p1 == [0,0])
                 deltaX = p1[0] - p2[0]
                 deltaY = p1[1] - p2[1]
-                print(f'deltax : {deltaX}, deltaY {deltaY}')
-                if deltaY == 0:
+                if deltaX == 0:
                     continue
-                slope = deltaX / deltaY
+                slope = deltaY / deltaX
                 # y - mx = b
-                intercept = p1[1] - slope * p1[0]
+                intercept = Fraction(p1[1]) - slope * Fraction(p1[0])
                 lines.add( (slope , intercept) )
         # make lines from all points to origin
         return lines
 
 sol = Solution()
-#print(sol.maxPoints(points =[[1,1],[2,2],[3,3]]))
-#print(sol.maxPoints(points =[[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]))
+print(sol.maxPoints(points =[[1,1],[2,2],[3,3]]))
+print(sol.maxPoints(points =[[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]))
 print(sol.maxPoints(points =[[1,0],[0,0]]))
+print(sol.maxPoints(points =[[2,3],[1,1]]))
+points = [[-6,-1],[3,1],[12,3]]
+print(Solution().maxPoints(points))

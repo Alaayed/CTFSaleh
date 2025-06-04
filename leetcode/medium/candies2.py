3caeef2edb87ed368fda76c899ec9f85787f984b
Alaayed <salaayed@purdue.edu>
2025-06-04 09:57:04 +0300

    Finished candies and atoi. Candies was very annoying at the beginning, but I found it interesting when I really put my head to it. Did atoi in C after finding it my attempted but not completed---I feel like I avenged 2023 Saleh's frustration.

--- leetcode/medium/candies2.py
+++ leetcode/medium/candies2.py
@@ -1,13 +1,23 @@
+from math import comb
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        # 2 cases 
-        # limit >= n 
-        permutations=0
-        for i in range(n): 
-            if (n-i) > 0:
-                for j in range (n-i):
-            else:
-                permutations+=1 
-            for k in range (n-i-j):
-        # limit < n
-        return 1
+        # Stars and bars theorem 
+        if (n < limit): 
+            return comb(n+3-1 , 3-1)
+
+        inc = 0
+        for i in range (0, min(limit, n) + 1):
+            # Has a lower and upper range
+            remaining_stock = n - i
+            min_range = max(remaining_stock - limit, 0) 
+            max_range = min(limit , remaining_stock)
+            number_of_solutions = max(max_range - min_range + 1 , 0)
+            inc += number_of_solutions
+            print(f'inc: {inc}')
+        return inc
+    
+sol = Solution()
+#print(sol.distributeCandies( 5, 2))
+#print(sol.distributeCandies(3,3))
+#print(sol.distributeCandies(10001,20001))
+print(sol.distributeCandies(4,1))

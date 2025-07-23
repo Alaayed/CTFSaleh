34f37afde249a9df0a6a9c4784f15e8674ee54e3
Alaayed <salaayed@purdue.edu>
2025-07-23 17:17:08 +0300

    Made this way harder than I had to. Twice. 2 hours of my life, wasted because I didnt have the simple idea of just doing two passes.

---
+++ leetcode/medium/maximumScoreFromRemovingSubstring.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import *
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        def remove_pair(s: str, first: str, second: str, value: int) -> Tuple[str, int]:
+            stack = []
+            score = 0
+            for ch in s:
+                if stack and stack[-1] == first and ch == second:
+                    stack.pop()
+                    score += value
+                else:
+                    stack.append(ch)
+            return ''.join(stack), score
+
+        total = 0
+        if x > y:
+            # Remove 'ab' first
+            s, gain = remove_pair(s, 'a', 'b', x)
+            total += gain
+            s, gain = remove_pair(s, 'b', 'a', y)
+            total += gain
+        else:
+            # Remove 'ba' first
+            s, gain = remove_pair(s, 'b', 'a', y)
+            total += gain
+            s, gain = remove_pair(s, 'a', 'b', x)
+            total += gain
+
+        return total

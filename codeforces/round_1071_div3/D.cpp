870aa8e58705057b28f19dba7b8b9d618760edfd
Saleh Alaayed FatherPC <salaayed@purdue.edu>
2025-12-25 17:37:19 +0300

    finished problem D.

--- codeforces/round_1071_div3/D.cpp
+++ codeforces/round_1071_div3/D.cpp
@@ -7,8 +7,28 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
+	int t,n;
+	cin >> t;
+	rep(k , 0 , t) {
+		cin >> n;
+		vector<int>	arr;
+		rep (i, 0, n+1) {
+			arr.push_back(  (1 << (n-i)) - 1);
+			// 000
+			// 111
+			// 101
+			// Condition implies: j can be represented using i bits
+			for (int j = 2; j < ((1 << i) - 1); j+=2 ) {
+				// set the lowest n-i bits, then set all possible permutations of the top i most bits (excluding 1 repeating)
+				arr.push_back( (1<< n-i) - 1 | (j << n-i));
+			}
+		}
+		for (int a : arr) {
+			cout << a << ' ';
+		}
+		cout << '\n';
+	}
 }
--- codeforces/round_1071_div3/D.py
+++ codeforces/round_1071_div3/D.py
@@ -6,14 +6,7 @@ def solve():
         arr = []
         for i in range(n):
             arr.append((1<<(n-i))-1)
-        for i in range(2, 1 << n):
-            # checks if i == 2**?-1
-            if not (i.bit_count() == i.bit_length()) and (i % 2 == 1):
-                arr.append(i)
-        arr.append(0)
-        for i in range(1, 1 << n):
-            if not  (i.bit_count() == i.bit_length()) and not(i % 2 == 1):
-                arr.append(i)
+        
 
         print(' '.join(list(map(str, arr))))
 solve()

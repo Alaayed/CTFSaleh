9c1cd80525d28592370d12924f8fa4a5735e5a1a
Alaayed <112523580+Alaayed@users.noreply.github.com>
2026-01-19 21:53:36 -0500

    1- Processed data token by token. 2- Used path compression to avoid recursion runtime errors. Spoilers, it was the recursion that got me.

--- cp3/topic0/C.py
+++ cp3/topic0/C.py
@@ -1,41 +1,47 @@
-def find(x, parent):
-    if parent[x] != x:
-        parent[x] = find(parent[x], parent)
+import sys
+def find(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent, parent[x])
     return parent[x]
-
-def union(x, y, parent):
-    rootX = find(x, parent)
-    rootY = find(y, parent)
-    if rootX != rootY:
-        parent[rootY] = rootX
-def is_connected(edges, n):
-    parent = list(range(n + 1))
-    for u, v in edges:
-        union(u, v, parent)
-    
-    root = find(1, parent)
-    for i in range(2, n + 1):
-        if find(i, parent) != root:
-            return False
-    return True
+def union(parent, rank, x, y):
+    rx = find(parent, x)
+    ry = find(parent, y)
+    if rx == ry:
+        return
+    if rank[rx] < rank[ry]:
+        parent[rx] = ry
+    elif rank[ry] < rank[rx]:
+        parent[ry] = rx
+    else:
+        parent[ry] = rx
+        rank[rx] += 1
+def is_connected(edges, n ):
+    parent  = [i for i in range(n+1)]
+    rank    = [1 for _ in range(n+1)]
+    # Add all edges
+    for u,v in edges:
+        union(parent, rank, u, v)
+    root = find(parent, 1)
+    return all(find(parent, i) == root for i in range(1,n+1))
 
 def solve():
-    t = int(input())
+    it = iter(map(int, sys.stdin.read().strip().split()))
+    t = next(it)
     for _ in range(t):
         # read and ignore blank lines
-        _ = input() 
-        n, m = map(int , input().split())
+        n,m = next(it), next(it)
         # construct the adj list
         edges = []
         for _ in range(m):
-            u, v, w = map(int , input().split())
+            u, v, w = next(it), next(it), next(it) 
             edges.append((u , v , w))
         bestWeight = (1 << 30) - 1
         for i in range(30):
             topbit = 1 << (29 - i)
             candWeight = bestWeight ^ topbit
             filteredEdges = [(u, v) for u, v, w in edges if (w & candWeight) == w]
-            if is_connected(filteredEdges, n):
+            if is_connected(filteredEdges, n):  
                 bestWeight = candWeight
         print(bestWeight)
 solve()

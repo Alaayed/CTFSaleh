29b04a1efe99c1475ad8b5e930a8d7c633c70a6f
Alaayed <salaayed@purdue.edu>
2026-01-28 16:01:29 -0500

    Swaps in python were not fast enough. I'll retry in c++

--- cp3/practice_topic2/Modulo Sum.py
+++ cp3/practice_topic2/Modulo Sum.py
@@ -9,22 +9,29 @@
 # We visit each state at most once
 # |S| = 10^6 * 10^3 = 10^9
 # it's within bounds
+
+# First submit
+
 # submitted, crashed on test 16 from memory limit failure
 # Ah, just realized that was a gigabyte of data the states were storing.
 # If you imagine it as a column, I just need two columns.
 # The "current" column--the one thats being updated-- and the previous one.
 # still 10^9 computations but only 2000 states tracked
 # could probably use a python style swap function for fast swaps
+
+# second submit
+
+# TLE on test 16. The swaps just aren't fast enough.
+# I will try reimplementing in c++ to see if that fixes it
 def solve():
 	n,m = map(int, input().split())
 	# make nums 1 indexed, for simplicity and uniformity
 	nums = [0] + list(map(int, input().split()))
-	dp = [ [False for _ in range(m)] for _ in range(n+1)]
-	# Base cases
+	current  = [0 for _ in range(m)]
 	for i in range(1, n+1):
-		dp[i][nums[i] % m] = True
-	for i in range(1, n+1):
+		previous = current
+		current = [0 for _ in range(m)]
 		for j in range(0, m):
-			dp[i][j] = max(dp[i-1][j] , dp[i-1][(j - nums[i])% m], dp[i][j])
-	print('YES' if dp[n][0]  else 'NO')
+			current[j] = max(previous[j] , previous[(j - nums[i])% m], (nums[i]%m) == j)
+	print('YES' if current[0]  else 'NO')
 solve()

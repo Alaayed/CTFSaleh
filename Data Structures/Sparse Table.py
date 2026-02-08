b18be23d1647b49a5bd111c90be880e29c3691a2
Alaayed <salaayed@purdue.edu>
2026-02-08 16:33:48 -0500

    MLE, need a more efficient approach

--- Data Structures/Sparse Table.py
+++ Data Structures/Sparse Table.py
@@ -1,44 +1,35 @@
-import random
-import numpy as np
-class SparseTable:
-	def __init__(self, array):
-		self.array = array
-		self.n = len(array)
-		self.table = [ [0 for _ in range(self.n.bit_length())] for _ in range(len(array)) ]
-		self.make_table()
-	def make_table(self):
-		for i in range(len(self.table)): # Base case
-			self.table[i][0] = self.array[i]
-		# table[i][j]: solution for range query starting from i and extending 2**j
-		# Guarantees logarithmic speed for most range queries
-		# And constant speed for idempotent operations
-		for j in range(1,len(self.table[0])):
-			for i in range(self.n - (1 << j)+1): # ensure range fits in 1..n
-				srs =i + (1 << j-1) # second range start
-				self.table[i][j] = self._operation(self.table[i][j-1] ,
-				                       self.table[srs][j-1])
-	def _largest_bit_position(self, num): # zero indexed
-		return num.bit_length()-1
-	def _operation(self, l, r): # operation we use
-		return max(l,r)
-	def range_query(self,l,r, idempotent=True):
+
+class JaggedSparseTable:
+	def __init__(self, a):
+		self.st = [a[:]]
+		n = len(a)
+		k = 1
+		while (1 << k) <= n:
+			prev = self.st[-1]
+			step = 1 << (k - 1)
+			cur = [
+				max(prev[i], prev[i + step])
+				for i in range(n - (1 << k) + 1)
+			]
+			self.st.append(cur)
+			k += 1
+
+	def query(self, l, r, idempotent=True):
 		if idempotent:
-			range_length = r-l+1
-			bit_position = self._largest_bit_position(range_length)
-			new_r = r- (1<< bit_position)+1
-			res = self._operation(self.table[l][bit_position]
-			                      ,self.table[new_r][bit_position])
-			return res
-		# imp later
+			length = r - l + 1
+			k = length.bit_length() - 1
+			return max(self.st[k][l], self.st[k][r - (1 << k) + 1])
+import numpy as np
+import random
 class SparseTester:
 	def __init__(self):
 		pass
 	def mid_test(self, tests):
 		array = np.random.randint(0,100,1_000).tolist()
-		table = SparseTable(array)
+		table = JaggedSparseTable(array)
 		for _ in range (tests):
 			l,r = sorted([random.randint(0,999) for _ in range(2)])
-			if max(array[l:r+1]) != table.range_query(l,r,idempotent=True):
+			if max(array[l:r+1]) != table.query(l,r,idempotent=True):
 				print('ISSUE DETECTED')
 		print('NO ISSUES')
 tester = SparseTester()
--- cp3/topic3/C.py
+++ cp3/topic3/C.py
@@ -1,13 +1,54 @@
 # link: https://codeforces.com/group/xSQ0pfWy2O/contest/668150/problem/C
-class SparseTable:
+# Okay, when moving from the jth to jth+1 column,
+# We don't have to recompute the max for all the n sliding arrays
+# if we have to recompute the value for a sliding array of length k,
+# we have to recompute the value for all sliding arrays of length >= k
+# make all arrays sparse tables for O(1) max range queries
+# Keep a prefix sum for all values that don't need to be recomputed (all values reachable)
 
-def contribution(i,j,nums):
+class JaggedSparseTable:
+	def __init__(self, a):
+		self.st = [a[:]]
+		n = len(a)
+		k = 1
+		while (1 << k) <= n:
+			prev = self.st[-1]
+			step = 1 << (k - 1)
+			cur = [
+				max(prev[i], prev[i + step])
+				for i in range(n - (1 << k) + 1)
+			]
+			self.st.append(cur)
+			k += 1
+
+	def query(self, l, r, idempotent=True):
+		if idempotent:
+			length = r - l + 1
+			k = length.bit_length() - 1
+			return max(self.st[k][l], self.st[k][r - (1 << k) + 1])
 
 def solve():
 	n,w = map(int,input().split())
-	nums = [list(map(int,input().split())) for _ in range(n)]
-	print(*nums)
-	#dp[j][i] =
-	#sum(dp[j])
-
+	nums = [list(map(int,input().split()))[1:] for _ in range(n)]
+	nums = sorted(nums, key=lambda x: len(x))
+	tables = [JaggedSparseTable(num) if len(nums) > 2 else None for num in nums]
+	res = []
+	for j in range(w):
+		jth_sum = 0
+		# For the jth column
+		for i in range(n):
+			# contribution of the i'th column
+			arr_len = len(nums[i])
+			l = max(arr_len-(w-j), 0)
+			r = min(j, arr_len-1)
+			if tables[i]: ith_contribution = tables[i].query(l,r)
+			else: ith_contribution = max(nums[i][l:r+1])
+			if ith_contribution < 0:
+				if arr_len - (w-j) < 0:
+					continue
+				if arr_len-1 < j:
+					continue
+			jth_sum += ith_contribution
+		res.append(jth_sum)
+	print(*res)
 solve()
